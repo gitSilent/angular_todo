9a7e8fa2d971d70e2a4fad8d4437505af8d95674
Dmitry <dmitrym2019@mail.ru>
2023-03-02 21:55:34 +0300

    deleted task folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Task } from './todo-body/task/task.component';
 import { TodoBody } from './todo-body/todo-body.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button'
@@ -11,8 +10,7 @@ import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
     AppComponent,
-    TodoBody,
-    Task
+    TodoBody
   ],
   imports: [
     BrowserModule,
